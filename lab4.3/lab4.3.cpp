48cc6bfbb2cb535cfed96aa777e5f1be9fd1c6fb
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-10-21 15:24:00 +0300

    lab4.4

---
+++ lab4.3/lab4.3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, F, b, c, a;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x < 0 && b != 0)
+			F = a * x * x + b;
+		else
+			if (x > 0 && b == 0)
+				F = (x - a) / (x - c);
+			else
+				F = x / c;
+		x += dx;
+	}
+	cout << " F = " << F << endl;
+	return 0;
+}
